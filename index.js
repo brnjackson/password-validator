e2f0f67c71094db68c0b09cf29926d43c57c39a3
Brittney Jackson <brnjackson@yahoo.com>
2021-07-13 21:14:23 -0400

    started password validate function, wrote statement to check length of password

---
+++ index.js
@@ -0,0 +1,28 @@
+function validatePassword(password) {
+  if (password.length < 8) {
+    return false
+  }
+
+  return true
+}
+
+let lowerCaseCount = 0
+let upperCaseCount = 0
+let numberCount = 0
+let specialCharacterCount = 0
+
+for(i = 0; i < password.length; i++) {
+  let currentLetter = password[i]
+
+  if (currentLetter === a lowercase)
+    lowerCaseCount++
+}
+
+if
+module.exports = validatePassword
+
+// needs 8 characters
+// needs one lowercase
+// needs one uppercase
+// needs one number
+// needs one special character
